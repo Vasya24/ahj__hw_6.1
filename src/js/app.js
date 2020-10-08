78b2c53f2881f9113fc2924f769a9a336f8b098e
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-10-08 15:41:56 +0300

    Что-то сделал, но ничего не понятно

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,31 @@
-// TODO: write code here
+let cards = document.getElementsByClassName('task_card');
+let lists = document.getElementsByClassName('task_list')
+for (let card of cards) {
+card.addEventListener('dragstart', (e) => {
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('Text', e.target.getAttribute('id'))
+    e.dataTransfer.setDragImage(e.target,10,10);
+    return true;
+});
+
+card.addEventListener('dragend', () => {
+    console.log('Эх тыыыы')
+})
+
+}
+for (let list of lists) {
+list.addEventListener('dragenter', (e) => {
+    e.preventDefault();
+    return true
+    
+});
+list.addEventListener('dragover', (e) => {
+    e.preventDefault();
+})
+list.addEventListener('drop', (e) => {
+    let data = e.dataTransfer.getData('Text');
+    e.target.appendChild(document.getElementById(data));
+    e.stopPropagation();
+    return false
+})
+}
